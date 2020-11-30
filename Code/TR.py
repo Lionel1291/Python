2851b1e405010f31872ace34687e9db0cf53313c
Lionel1291 <lionellee1@icloud.com>
2020-11-30 15:54:01 +0100

    Com User PW
    Change User PW

---
+++ Code/TR.py
@@ -0,0 +1,7 @@
+a = 10
+b = 12
+operator = (input("Please enter operator:"))
+if operator == "*":
+    print(a*b)
+elif operator == "+":
+    print(a+b)
--- Code/Windows/ChangeUserThings.py
+++ Code/Windows/ChangeUserThings.py
@@ -3,9 +3,9 @@ from pathlib import Path
 users = [x.name for x in Path(r'C:\Users').glob('*') if x.name not in ['Default', 'Default User', 'Public', 'All Users'] and x.is_dir()]
 
 for user in users:
-    if(user == 'testuser'):
+    if(user == 'thacom'):
         try:
-            data = subprocess.call(['net', 'user', user, '*']).decode('cp850')
+            data = subprocess.call(['net', 'user', user, 'muster']).decode('cp850')
             print('password')
             print('password')
         except IndexError:
