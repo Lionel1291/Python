9b16a29a640737bc800645b7bcc3bd9d8c94c2e8
Lionel1291 <lionellee1@icloud.com>
2020-12-08 15:21:02 +0100

    Update HelloWorld.py

---
+++ Code/FuerUnterricht/HelloWorld.py
@@ -0,0 +1,9 @@
+variable = "variable"
+array = ['Keine ;','Lol']
+print(array)
+if array[0] == 'Keine ;':
+    print('Success')
+elif variable != 'Keine ;':
+    print(variable)
+else:
+    print('Denie')
