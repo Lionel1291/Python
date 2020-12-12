3f96a273b4fc00951e4f165c7420cd7934aa6762
Lionel1291 <lionellee1@icloud.com>
2020-12-12 19:51:55 +0100

    Com

--- Code/FuerUnterricht/HelloWorld.py
+++ Code/FuerUnterricht/HelloWorld.py
@@ -1,9 +1,29 @@
 variable = "variable"
 array = ['Keine ;','Lol']
 print(array)
+print(len(array))
 if array[0] == 'Keine ;':
     print('Success')
 elif variable != 'Keine ;':
     print(variable)
 else:
     print('Denie')
+
+def funktion():
+    string = str("hallo")
+    boolean = bool(True)
+    boolean = bool(False)
+    floatf = float(12.12010)
+    null = None
+    return
+
+boolean = bool(10 == 11)
+print(boolean)
+print("Gibt False zurück\t"+str(boolean))
+
+boolean2 = bool(1)
+print("Gibt True zurück\t"+str(boolean2))
+
+boolean3 = bool(0)
+print("Gibt False zurück\t"+str(boolean3))
+
